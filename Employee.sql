14a39b532b5007eec4e231487838b2817d07907e
vivek <ikumarvivek786@gmail.com>
2022-11-03 23:20:15 +0530

    UC4_RetriveData

--- Employee.sql
+++ Employee.sql
@@ -4,5 +4,7 @@ create database Payroll_Service
 create table EmployeePaayroll(ID int identity(1,1)primary key,Empname varchar(200),salary bigint,Startdate date)
 --UC-3 insert record
 insert into EmployeePaayroll values ('Neha',45000,'2018-01-01','F')
+--UC-4 retrive data
+select * from EmployeePaayroll
 
 

e2495099cfe9ca51e7d0b1c6ac9b3b56ebf16f71
vivek <ikumarvivek786@gmail.com>
2022-11-03 23:17:26 +0530

    UC3_InsertRecord

--- Employee.sql
+++ Employee.sql
@@ -2,5 +2,7 @@
 create database Payroll_Service
 --UC-2 create table
 create table EmployeePaayroll(ID int identity(1,1)primary key,Empname varchar(200),salary bigint,Startdate date)
+--UC-3 insert record
+insert into EmployeePaayroll values ('Neha',45000,'2018-01-01','F')
 
 

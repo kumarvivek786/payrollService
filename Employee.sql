4c3e167a66bee0180ef1d28989ccf9ef3a83579e
vivek <ikumarvivek786@gmail.com>
2022-11-03 23:41:53 +0530

    UC7_FindMax_Min_Sum_Avg

--- Employee.sql
+++ Employee.sql
@@ -12,5 +12,10 @@ select * from EmployeePaayroll where Startdate between CAST('2018-01-01' As date
 alter table EmployeePayroll add Gender char(1)
 update EmployeePayroll set Gender='M' Where Empname='vivek'
 update EmployeePayroll set Gender='M' Where Empname='charlie'
+--UC-7
+select Sum(salary)as Total_sum from EmployeePaayroll where Gender='M' group by Gender
+select avg(salary)as Avgsalary from EmployeePaayroll where Gender='M' group by Gender
+select min(salary)as Minsalary from EmployeePaayroll where Gender='M' group by Gender
+select count(*)as totalrecord from EmployeePaayroll group by gender
 
 

f23236fc8acdd9ac9be6e2721bc7e132aa00fc3b
vivek <ikumarvivek786@gmail.com>
2022-11-03 23:14:45 +0530

    UC2_CreateTable

--- Employee.sql
+++ Employee.sql
@@ -1,4 +1,6 @@
 --UC-1 create database
 create database Payroll_Service
+--UC-2 create table
+create table EmployeePaayroll(ID int identity(1,1)primary key,Empname varchar(200),salary bigint,Startdate date)
 
 

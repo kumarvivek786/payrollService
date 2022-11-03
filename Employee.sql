70674432169c69b347bb3df733dc3e52acfe7d52
vivek <ikumarvivek786@gmail.com>
2022-11-03 23:32:34 +0530

    UC5_RetriveParticulerRecord

--- Employee.sql
+++ Employee.sql
@@ -3,8 +3,11 @@ create database Payroll_Service
 --UC-2 create table
 create table EmployeePaayroll(ID int identity(1,1)primary key,Empname varchar(200),salary bigint,Startdate date)
 --UC-3 insert record
-insert into EmployeePaayroll values ('Neha',45000,'2018-01-01','F')
+insert into EmployeePaayroll values ('Neha',45000,'2018-01-01')
+insert into EmployeePaayroll values ('Vivek',46000,'2020-01-01')
 --UC-4 retrive data
 select * from EmployeePaayroll
+--UC-5 
+select * from EmployeePaayroll where Startdate between CAST('2018-01-01' As date) and getdate()
 
 
